25d551cd8d3bf37584b9a416deda21ef3a86daea
Monique Moreira <mmm@ic.ufal.br>
2025-09-19 22:17:58 -0300

    feat: :sparkles: Add de return_status para inicialização de modulos

---
+++ src/return_status.c
@@ -0,0 +1,22 @@
+#include "return_status.h"
+
+static const char* RETURN_STATUS_MESSAGES[] = {
+    "OK",
+    "Error",
+    "Not Initialized",
+    "Timeout",
+    "Not Found",
+    "Invalid Parameter"
+};
+
+inline const char* RETURN_STATUS_Module(RETURN_STATUS_t status) {
+    switch (status) {
+        case RETURN_STATUS_OK: return RETURN_STATUS_MESSAGES[0];
+        case RETURN_STATUS_ERROR: return RETURN_STATUS_MESSAGES[1];
+        case RETURN_STATU_NOT_INICIALIZED: return RETURN_STATUS_MESSAGES[2];
+        case RETURN_STATUS_TIMEOUT: return RETURN_STATUS_MESSAGES[3];
+        case RETURN_STATUS_NOT_FOUND: return RETURN_STATUS_MESSAGES[4];
+        case RETURN_STATUS_INVALID_PARAM: return RETURN_STATUS_MESSAGES[5];
+        default: return "Unknown Status";
+    }
+}
