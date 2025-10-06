446e6e3bafb2a380748fba882005ef86a2f12766
Monique Moreira <mmm@ic.ufal.br>
2025-10-05 22:31:15 -0300

    refactor: :recycle: Add da chamada de IO_MANAGER_Init(), WATCHDOG_Init() e remoção de setup

--- src/Water_Time.c
+++ src/Water_Time.c
@@ -1,37 +1,17 @@
+#include "app/setup/setup.h"
 #include "itf/logger/logger.h"
 #include "svc/timer/timer.h"
+#include "svc/io_manager/io_manager.h"
+
 #include "pico/stdlib.h"
 #include <stdio.h>
 
 #include "hardware/uart.h"
 #include "FreeRTOS.h"
 
-#define LED_PIN 25
-#define TIMEOUT_MS 10000
-#define TIMEOUT_SHORT_MS 2000
-
 TaskHandle_t xTask_Init = NULL;
 
 static void __Init_Task__(void *params);
-
-void setup_task(void *pvParameters) {
-    (void) pvParameters;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 0);
-    
-    TIMER_Start("led_timer", 1000); // 1 segundo
-
-    while(1) {
-        if(TIMER_Check("led_timer") == TIMER_EXPIRED) {
-            printf("LED Timer Expirou!\n");
-            // decide quando reiniciar
-            TIMER_Reset("led_timer");
-            gpio_put(LED_PIN, !gpio_get(LED_PIN)); // toggle LED
-        }
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-}
 
 int main() {
     stdio_init_all();
@@ -57,8 +37,20 @@ static void __Init_Task__(void *params) {
         LOGGER_Info("SYSTEM", "Erro ao iniciar timer!");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
     }
+    if (SETUP_Init() != RETURN_STATUS_OK) {
+        LOGGER_Info("SYSTEM", "Erro ao iniciar Setup!");
+        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    if (IO_MANAGER_Init() != RETURN_STATUS_OK) {
+        LOGGER_Info("SYSTEM", "Erro ao iniciar IO Manager!");
+        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    if(WATCHDOG_Init() != RETURN_STATUS_OK) {
+        LOGGER_Info("SYSTEM", "Erro ao iniciar Watchdog!");
+        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    
 
-    xTaskCreate(setup_task, "SetupTask", 256, NULL, 1, NULL);
     vTaskDelete(xTask_Init);
 
 }
