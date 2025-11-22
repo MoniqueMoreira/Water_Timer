7f0be66adca2c7c526985c4d87e87feec99ae5de
Monique Moreira <mmm@ic.ufal.br>
2025-11-21 21:36:44 -0300

    test: :test_tube: Teste para benchmark

--- src/Water_Time.c
+++ src/Water_Time.c
@@ -2,6 +2,7 @@
 #include "itf/logger/logger.h"
 #include "svc/timer/timer.h"
 #include "svc/io_manager/io_manager.h"
+#include "test/benchmark.h"
 
 #include "pico/stdlib.h"
 #include <stdio.h>
@@ -28,15 +29,16 @@ static void __Init_Task__(void *params) {
 
     printf("Iniciando sistema...\n");
 
-
-    if (LOGGER_Init(NULL) != RETURN_STATUS_OK) {
-        printf("Erro ao iniciar logger!\n");
-        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
-    }
     if (TIMER_Init() != RETURN_STATUS_OK) {
         LOGGER_Info("SYSTEM", "Erro ao iniciar timer!");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
     }
+
+    /*if (LOGGER_Init(NULL) != RETURN_STATUS_OK) {
+        printf("Erro ao iniciar logger!\n");
+        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    
     if (SETUP_Init() != RETURN_STATUS_OK) {
         LOGGER_Info("SYSTEM", "Erro ao iniciar Setup!");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
@@ -45,12 +47,14 @@ static void __Init_Task__(void *params) {
         LOGGER_Info("SYSTEM", "Erro ao iniciar IO Manager!");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
     }
+    
     if(WATCHDOG_Init() != RETURN_STATUS_OK) {
         LOGGER_Info("SYSTEM", "Erro ao iniciar Watchdog!");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    }*/
     
 
     vTaskDelete(xTask_Init);
 
 }
+
