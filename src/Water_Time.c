0e91abc8231c8086fcf5e8382f55ed7e9f34bcd3
Monique Moreira <mmm@ic.ufal.br>
2025-11-26 09:34:39 -0300

    feat: :sparkles:
    Add de módulos de device_settings, e flash.

--- src/Water_Time.c
+++ src/Water_Time.c
@@ -1,14 +1,16 @@
-#include "app/setup/setup.h"
-#include "itf/logger/logger.h"
-#include "svc/timer/timer.h"
-#include "svc/io_manager/io_manager.h"
-#include "test/benchmark.h"
-
 #include "pico/stdlib.h"
 #include <stdio.h>
 
 #include "hardware/uart.h"
 #include "FreeRTOS.h"
+
+#include "app/setup/setup.h"
+#include "itf/logger/logger.h"
+#include "itf/flash_mem/flash_mem.h"
+#include "svc/device_settings/device_settings.h"
+#include "svc/timer/timer.h"
+#include "svc/io_manager/io_manager.h"
+#include "test/benchmark.h"
 
 TaskHandle_t xTask_Init = NULL;
 
@@ -19,7 +21,7 @@ int main() {
 
     sleep_ms(2000); // aguarda conexão serial
     
-    xTaskCreate(__Init_Task__, "Init", 1024, NULL, 2, &xTask_Init);
+    xTaskCreate(__Init_Task__, "Init", 2*1024, NULL, 2, &xTask_Init);
 
     vTaskStartScheduler();
 }
@@ -29,30 +31,34 @@ static void __Init_Task__(void *params) {
 
     printf("Iniciando sistema...\n");
 
-    if (TIMER_Init() != RETURN_STATUS_OK) {
-        LOGGER_Info("SYSTEM", "Erro ao iniciar timer!");
-        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-
-    /*if (LOGGER_Init(NULL) != RETURN_STATUS_OK) {
+    if (LOGGER_Init(NULL) != RETURN_STATUS_OK) {
         printf("Erro ao iniciar logger!\n");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
     }
-    
-    if (SETUP_Init() != RETURN_STATUS_OK) {
-        LOGGER_Info("SYSTEM", "Erro ao iniciar Setup!");
+    if(FLASH_Init() != RETURN_STATUS_OK) {
+        LOGGER_Info("SYSTEM", "Erro ao iniciar flash!");
+        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    if (DEVICE_SETTINGS_Init() != RETURN_STATUS_OK) {
+        LOGGER_Info("SYSTEM", "Erro ao iniciar Device Settings!");
+        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    if (TIMER_Init() != RETURN_STATUS_OK) {
+        LOGGER_Info("SYSTEM", "Erro ao iniciar timer!");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
     }
     if (IO_MANAGER_Init() != RETURN_STATUS_OK) {
         LOGGER_Info("SYSTEM", "Erro ao iniciar IO Manager!");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
     }
-    
+    if (SETUP_Init() != RETURN_STATUS_OK) {
+        LOGGER_Info("SYSTEM", "Erro ao iniciar Setup!");
+        while (1) vTaskDelay(pdMS_TO_TICKS(1000));
+    }
     if(WATCHDOG_Init() != RETURN_STATUS_OK) {
         LOGGER_Info("SYSTEM", "Erro ao iniciar Watchdog!");
         while (1) vTaskDelay(pdMS_TO_TICKS(1000));
-    }*/
-    
+    }
 
     vTaskDelete(xTask_Init);
 
